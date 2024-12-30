2a0a3b549411295db1638fae9a9056bbcd012b8c
SjxSubham <sjxsubham@gmail.com>
2024-12-30 21:34:20 +0530

    zx

---
+++ src/app/(root)/page.tsx
@@ -0,0 +1,13 @@
+export default function Home() {
+    return (
+      <div className="min-h-name">
+        <div className="max-w-[1800px] mx-auto px-4">
+         <div>header</div>
+          <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
+            editor and preview
+          </div>
+       </div> 
+      </div>
+    );
+  }
+  
