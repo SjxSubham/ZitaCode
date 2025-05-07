3911c1a59388d9c86023492ac3e19cab0c0af815
SjxSubham <sjxsubham@gmail.com>
2025-05-08 01:43:46 +0530

    snippets logs

---
+++ src/app/snippets/snippets.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const snippets = () => {
+  return (
+    <div className='bg-gray-300 shadow-2xl'>
+    <div className='font-extrabold text-3xl justify-center text-center p-9 '>This page is under maintainence</div>
+    </div>
+  )
+}
