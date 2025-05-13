84e1b32d50a3ec289a50afc3a1b892417a0c7ec7
SjxSubham <sjxsubham@gmail.com>
2025-05-13 20:36:53 +0530

    Network active, inactive status toast added

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,11 +1,13 @@
 import EditorPanel from "./_components/EditorPanel";
 import Header from "./_components/Header"
+import NetworkStatusToast from "./_components/NetworkStatusToast";
 import OutputPanel from "./_components/OutputPanel";
 
 export default function Home() {
     return (
       <div className="min-h-name">
         <div className="max-w-[1800px] mx-auto px-4">
+        <NetworkStatusToast />
          <Header/>
           <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
            <EditorPanel/>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import { ClerkProvider } from "@clerk/nextjs";
 import ConvexClientProvider from "@/components/providers/ConvexClientProvider";
 import Footer from "@/components/Footer";
 import { ThemeProvider } from "@/components/ThemeContext";
-
+import { Toaster } from "react-hot-toast";
 
 
 const geistSans = localFont({
@@ -37,6 +37,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen
          bg-gradient-to-b from-gray-900 to-gray-950 text-gray-100 flex flex-col`}
       >
+        <Toaster position="top-center" />
         <ThemeProvider>
         <ConvexClientProvider>
        {children}
