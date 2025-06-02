98ee381ae084c29a282d5b0bad97f6a988f94f82
SjxSubham <sjxsubham@gmail.com>
2025-06-03 00:02:08 +0530

    Refactor ThemeSelector component for cleaner className formatting

--- src/app/(root)/_components/ThemeSelector.tsx
+++ src/app/(root)/_components/ThemeSelector.tsx
@@ -49,7 +49,7 @@ function ThemeSelector() {
         whileHover={{ scale: 1.02 }}
         whileTap={{ scale: 0.98 }}
         onClick={() => setIsOpen(!isOpen)}
-        className="w-48 group relative flex justify-between items-center gap-2 px-2 py-2.5 bg-gray-300 dark:bg-[#1e1e2e]/80 dark:hover:bg-[#262637] 
+        className="w-48 group relative flex justify-between items-center gap-2 px-2 py-2.5 bg-gray-300 dark:bg-[#1e1e2e]/80  
         rounded-lg transition-all shadow-lg duration-200 border dark:border-gray-800/50 hover:border-gray-700"
       >
         {/* hover state bg decorator */}
