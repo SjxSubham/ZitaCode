bd7b8c57419422d72e864fc794f0b50ac9dca4c6
SjxSubham <sjxsubham@gmail.com>
2025-06-07 20:53:47 +0530

    fixed

--- convex/users.ts
+++ convex/users.ts
@@ -18,7 +18,7 @@ export const syncuser = mutation({
                 userId: args.userId,
                 email: args.email,
                 name: args.name,
-                isPro: true,  //remember change it to false, I have made this true for testing purpose for all users.
+                isPro: false,  //remember change it to false, I have made this true for testing purpose for all users.
             });
         }
     },
