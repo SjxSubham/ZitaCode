83987fcfac0147e49c8e245de2adcbe84eb937fe
SjxSubham <sjxsubham@gmail.com>
2025-06-14 08:52:15 +0530

    changed icon

--- src/app/(root)/_components/EditorPanel.tsx
+++ src/app/(root)/_components/EditorPanel.tsx
@@ -161,7 +161,7 @@ import { defineMonacoThemes, LANGUAGE_CONFIG } from "../_constants";
 import { Editor } from "@monaco-editor/react";
 import { motion } from "framer-motion";
 import Image from "next/image";
-import { RotateCcwIcon, ShareIcon, TypeIcon, Sparkles, BrainCircuit, WandSparkles } from "lucide-react";
+import { RotateCcwIcon, ShareIcon, TypeIcon, Sparkles, BrainCircuit, WandSparkles, ALargeSmall } from "lucide-react";
 import { useClerk } from "@clerk/nextjs";
 import { EditorPanelSkeleton } from "./EditorPanelSkeleton";
 import useMounted from "@/hooks/useMounted";
