09e1a31806767738c92eb6aed34f8b4633809630
SjxSubham <sjxsubham@gmail.com>
2025-09-27 00:32:32 +0530

    Update Sign In button style to use gradient background

--- src/app/(root)/_components/HeaderProfileBtn.tsx
+++ src/app/(root)/_components/HeaderProfileBtn.tsx
@@ -25,7 +25,7 @@ function HeaderProfileBtn() {
     </div>
     <SignedOut>
       <SignInButton>
-        <button className="bg-blue-600 text-white hover:bg-blue-800 rounded-md px-4 py-2">
+        <button className="bg-gradient-to-r from-blue-600 to-blue-400  text-white hover:bg-blue-800 rounded-md px-4 py-2">
           Sign In
         </button>
       </SignInButton>
